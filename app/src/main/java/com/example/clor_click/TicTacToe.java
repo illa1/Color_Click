e0c8b7ddd3b9b3cbcd0136545ee0395cde24a298
Alex9003 <agl.luchko@gmail.com>
2026-02-14 14:12:59 +0200

    1402

--- app/src/main/java/com/example/clor_click/TicTacToe.java
+++ app/src/main/java/com/example/clor_click/TicTacToe.java
@@ -1,16 +1,50 @@
 package com.example.clor_click;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TicTacToe extends AppCompatActivity {
+    private TextView statusTextInfo;
+    private Button[] buttons = new Button[9];
+    private Button resetButton;
+
+    private boolean isPlayerX = true;
+    private int moveCount = 0;
+    private int[] gameStatus = {0,0,0,0,0,0,0,0,0,};
+    private final int[][] winningPositions = {
+            {0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}
+    };
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.tic_tac_toe);
+
+        statusTextInfo = findViewById(R.id.statusTextInfo);
+        resetButton = findViewById(R.id.resetButton);
+
+        for(int i = 0; i<9; i++){
+            String buttonID = "btn_tic_tac_toe_" + i;
+
+            int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+
+            buttons[i] = findViewById(resID);
+
+            buttons[i].setOnClickListener(v -> onGridButtonClick((Button) v, index)
+            );
+        }
+    }
+    private void onGridButtonClick(Button btn, int index){
+
     }
 }
 
